36d6827bdbc0085a0609be08693e50233f520f6c
Edgardo Luis Jimenez <edgardotecno@gmail.com>
2024-08-11 13:04:55 -0500

    Controling an input

--- src/app/pages/labs/labs.component.ts
+++ src/app/pages/labs/labs.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-labs',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './labs.component.html',
   styleUrl: './labs.component.css',
 })
@@ -27,6 +28,14 @@ export class LabsComponent {
     avatar: 'https://w3schools.com/howto/img_avatar.png',
   });
 
+  colorCtrl = new FormControl();
+
+  constructor() {
+    this.colorCtrl.valueChanges.subscribe((value) => {
+      console.log(value);
+    });
+  }
+
   clickHandler() {
     alert('Hola');
   }
@@ -47,6 +56,17 @@ export class LabsComponent {
     const newValue = input.value;
     this.person.update((prevState) => {
       return { ...prevState, age: parseInt(newValue, 10) };
+    });
+  }
+
+  changeName(event: Event) {
+    const input = event.target as HTMLInputElement;
+    const newValue = input.value;
+    this.person.update((prevState) => {
+      return {
+        ...prevState,
+        name: newValue,
+      };
     });
   }
 }

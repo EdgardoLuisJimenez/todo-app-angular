e87b2d985ff0392c6ef52741a925aa29988f4cba
Edgardo Luis Jimenez <edgardojimenezu@gmail.com>
2024-08-12 22:40:41 -0500

    Clases on Angular

--- src/app/pages/labs/labs.component.ts
+++ src/app/pages/labs/labs.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
-import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-labs',
@@ -31,6 +31,10 @@ export class LabsComponent {
   colorCtrl = new FormControl();
   widthCtrl = new FormControl(50, {
     nonNullable: true,
+  });
+  nameCtrl = new FormControl('Edgardo Luis', {
+    nonNullable: true,
+    validators: [Validators.required, Validators.minLength(3)],
   });
 
   constructor() {
